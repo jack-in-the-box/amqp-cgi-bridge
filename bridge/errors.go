db26f46e255ff32b3f6d856b1b2cb713ebf759d2
Nico <nicolas.petrini@xait.com>
2025-04-29 17:19:14 +0200

    debug mesages

--- bridge/errors.go
+++ bridge/errors.go
@@ -3,6 +3,6 @@ package bridge
 import "errors"
 
 var ErrProcessorInternal = errors.New("processor was not able to perform request")
-var ErrUnknownStatus = errors.New("processor was not able to read response status code")
+var ErrUnknownStatus = errors.New("processor was not able to read response status code from backend")
 var ErrProcessingError = errors.New("request to processing backend has failed (response status code 3xx or 4xx)")
 var ErrProcessingFailed = errors.New("message processing failed (response status code 5xx)")
